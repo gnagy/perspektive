dcb1ac1abf08489eb885047f31d7a33b18b29b7a
Gergely Nagy <greg@webhejj.hu>
2023-10-26 10:53:10 +0200

    Gradle: remove native target, comment out JS for now

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,26 +21,9 @@ kotlin {
             }
         }
     }
-    js {
-        browser {
-            commonWebpackConfig {
-                cssSupport {
-                    enabled.set(true)
-                }
-            }
-        }
-    }
-    val hostOs = System.getProperty("os.name")
-    val isArm64 = System.getProperty("os.arch") == "aarch64"
-    val isMingwX64 = hostOs.startsWith("Windows")
-    val nativeTarget = when {
-        hostOs == "Mac OS X" && isArm64 -> macosArm64("native")
-        hostOs == "Mac OS X" && !isArm64 -> macosX64("native")
-        hostOs == "Linux" && isArm64 -> linuxArm64("native")
-        hostOs == "Linux" && !isArm64 -> linuxX64("native")
-        isMingwX64 -> mingwX64("native")
-        else -> throw GradleException("Host OS is not supported in Kotlin/Native.")
-    }
+//    js {
+//        nodejs {}
+//    }
 
     sourceSets {
         val commonMain by getting
@@ -51,9 +34,7 @@ kotlin {
         }
         val jvmMain by getting
         val jvmTest by getting
-        val jsMain by getting
-        val jsTest by getting
-        val nativeMain by getting
-        val nativeTest by getting
+//        val jsMain by getting
+//        val jsTest by getting
     }
 }
