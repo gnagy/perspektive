17d03c53a568641c6eecb2e445e6265abc051dfe
Gergely Nagy <greg@webhejj.hu>
2023-10-29 11:43:29 +0100

    Convert from kotlin multiplatform to kotlin jvm project

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    kotlin("multiplatform") version "1.9.10"
+    kotlin("jvm") version "1.9.10"
     id("org.jlleitschuh.gradle.ktlint") version "11.6.1"
     id("com.github.ben-manes.versions") version "0.49.0"
 }
@@ -11,36 +11,13 @@ repositories {
     mavenCentral()
 }
 
-kotlin {
-    jvm {
-        jvmToolchain(11)
-        withJava()
-        testRuns.named("test") {
-            executionTask.configure {
-                useJUnitPlatform()
-            }
-        }
-    }
-//    js {
-//        nodejs {}
-//    }
+dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("io.github.classgraph:classgraph:4.8.115")
+    implementation("net.sourceforge.plantuml:plantuml:1.2021.10")
+    testImplementation(kotlin("test"))
+}
 
-    sourceSets {
-        val commonMain by getting
-        val commonTest by getting {
-            dependencies {
-                implementation(kotlin("test"))
-            }
-        }
-        val jvmMain by getting {
-            dependencies {
-                implementation("org.jetbrains.kotlin:kotlin-reflect")
-                implementation("io.github.classgraph:classgraph:4.8.115")
-                implementation("net.sourceforge.plantuml:plantuml:1.2021.10")
-            }
-        }
-        val jvmTest by getting
-//        val jsMain by getting
-//        val jsTest by getting
-    }
+tasks.withType<Test> {
+    useJUnitPlatform()
 }
